a31e5492d3cc9427265e1f17a7f3097b6922af15
TTTyuqi <756683214@qq.com>
2020-10-11 21:19:29 +0800

    完成视频前台的界面搭建

---
+++ videomain/layouts/default.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      app
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-subheader class="mt-4 grey--text text--darken-1">我的关注</v-subheader>
+        <v-list>
+            <v-list-item
+              v-for="item in items2"
+              :key="item.text"
+              link
+            >
+              <v-list-item-avatar>
+                <img
+                  :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`"
+                  alt=""
+                >
+              </v-list-item-avatar>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item>
+          <v-list-item class="mt-4" link>
+            <v-list-item-action>
+              <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+            </v-list-item-action>
+            <v-list-item-title class="grey--text text--darken-1">Browse Channels</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon color="grey darken-1">mdi-cog</v-icon>
+            </v-list-item-action>
+            <v-list-item-title class="grey--text text--darken-1">Manage Subscriptions</v-list-item-title>
+          </v-list-item>
+          </v-list>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar 
+    clipped-left 
+    app 
+    color="red"
+    dense>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+       <v-icon
+          class="mx-4"
+          large
+        >
+          mdi-youtube
+        </v-icon>
+        <v-toolbar-title class="mr-12 align-center">
+          <span class="title">VlogShow</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-row
+          align="center"
+          style="max-width: 650px"
+        >
+          <v-text-field
+            :append-icon-cb="() => {}"
+            placeholder="Search..."
+            single-line
+            append-icon="mdi-magnify"
+            color="white"
+            hide-details
+          ></v-text-field>
+        </v-row>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: '首页',
+          to: '/',
+        },
+        {
+          icon: 'mdi-trending-up',
+          title: '热门视频',
+          to: '/HostVlog',
+        },
+        {
+          icon: 'mdi-youtube-subscription',
+          title: '我的视频',
+          to: '/inspire',
+        },
+       {
+          icon: 'mdi-history',
+          title: '历史记录',
+          to: '/inspire',
+        },
+      ],
+      items2: [
+        { picture: 28, text: 'Joseph' },
+        { picture: 38, text: 'Apple' },
+        { picture: 48, text: 'Xbox Ahoy' },
+        { picture: 58, text: 'Nokia' },
+        { picture: 78, text: 'MKBHD' },
+    ],
+    }
+  },
+}
+</script>
---
+++ videomain/pages/HostVlog.vue
@@ -0,0 +1,86 @@
+<template>
+    <v-hover>
+      <template v-slot="{ hover }">
+        <v-card
+        :elevation="hover ? 24 : 6"
+        class="mx-auto"
+        dark
+        max-width="400"
+        >
+            <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+            ></v-img>
+
+            <v-card-title>
+            Top western road trips
+            </v-card-title>
+
+            <v-card-subtitle>
+            1,000 miles of wonder
+            </v-card-subtitle>
+
+            <v-card-actions>
+        <v-list-item class="grow">
+            <v-list-item-avatar color="grey darken-3">
+            <v-img
+                class="elevation-6"
+                src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+            ></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+            <v-list-item-title>Evan You</v-list-item-title>
+            </v-list-item-content>
+
+            <v-row
+            align="center"
+            justify="end"
+            >
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+            <span class="subheading mr-2">256</span>
+            <span class="mr-1">·</span>
+            <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+            <span class="subheading">45</span>
+            </v-row>
+        </v-list-item>
+        </v-card-actions>
+    </v-card>
+     </template>
+    </v-hover>
+</template>
+
+<script>
+export default {
+    name:'HostVlog',
+    async asyncData({$axios}){
+        let query={
+            size:10,
+            page:1,
+            sort:{},
+            where:{}
+        }
+        const vlogList = await $axios.$get('/vlogs/vlog',{params:query})
+        // console.log('vlogList',vlogList)
+        return {
+            query,
+            vlogList
+        }
+    },
+    data () {
+        return {
+            
+        }
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
